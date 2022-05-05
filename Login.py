734287e59c64db8829db3d64b68361426fb319e4
GabooSinO <69467681+GabooSinO@users.noreply.github.com>
2022-05-05 11:37:50 +0200

    Update Login.py

--- Login.py
+++ Login.py
@@ -9,17 +9,44 @@ window.resizable(False, False)
 #----------------------------SAVE-----------------------------------------------
 
 def save():
-    savemail = mail.get()
+    savemail = mail2.get()
     saveuser = user.get()
     savepassword2 = password2.get()
-    file = open(r'''accounts.txt''', "w")
-    file.write(savemail)
-    file.write("//")
-    file.write(saveuser)
-    file.write("//")
-    file.write(savepassword2)
+    filemail = open(r'''accountmail.txt''', "w")
+    filemail.write(savemail)
+    filemail.close()
+    fileuser = open(r'''accountuser.txt''', "w")
+    fileuser.write(saveuser)
+    fileuser.close()
+    filepassword = open(r'''accountpassword.txt''', "w")
+    filepassword.write(savepassword2)
+    filepassword.close()
     savetext = Label(window2, text = "You are now registered")
     savetext.pack()
+
+def exit():
+    newwindow.destroy()
+
+def read():
+    global newwindow
+    global mail
+    global logintext
+    getmail = mail.get()
+    filemail = open(r'''accountmail.txt''', "r")
+    filepassword = open(r'''accountpassword.txt''', "r")
+    getpassword = password.get()
+    if getmail in filemail:
+        if getpassword in filepassword:
+            logintext['text'] = "Welcome"
+            newwindow = Toplevel(window)
+            newwindow.title("Program")
+            newwindow.geometry("200x200")
+            signout = Button(newwindow, text = "Sign out", command = exit)
+            signout.pack()
+        else:
+            logintext['text'] = "Incorrect password"
+    else:
+        logintext['text'] = "Mail not registered"
 
 #----------------------------INTERFACE------------------------------------------
 
@@ -40,13 +67,13 @@ passwordtext.pack()
 password = ttk.Entry(window,show = "*")
 password.pack(padx =10, pady =10,ipadx=100, ipady=10)
 
-login = Button(window, text = "Login")
+login = Button(window, text = "Login", command = read)
 login.pack(ipadx=25, ipady=10)
 
 mailtext2 = Label(window2, text = "Mail")
 mailtext2.pack()
-mail = Entry(window2)
-mail.pack(padx =10, pady =10, ipadx=100, ipady=10)
+mail2 = Entry(window2)
+mail2.pack(padx =10, pady =10, ipadx=100, ipady=10)
 
 usertext = Label(window2, text = "Nickname")
 usertext.pack()
@@ -61,5 +88,7 @@ password2.pack(padx =10, pady =10,ipadx=100, ipady=10)
 register = Button(window2, text = "Register", command = save)
 register.pack(ipadx=25, ipady=10)
 
+logintext = Label(window, text = " ")
+logintext.pack()
 
 window.mainloop()
